25160743948d817704898376f773c944b49f32eb
nick <nick@WORKGROUP.zxcv>
2016-04-01 00:00:32 +1300

    Made a form to add players and their details.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var pagedir = __dirname + '/pages';
 
 app.set('port', (process.env.PORT || 3434));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  //res.sendFile(__dirname + '/index.html');
-  res.send('<h1>Hello World</h1>');
+  res.sendFile(pagedir + '/playerdetails.html');
+  //res.send('<h1>Hello World</h1>');
 });
 
 http.listen(app.get('port'), function(){
