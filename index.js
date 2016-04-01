d223f788436e792688461adec601422af6adf8b7
nick <nick@WORKGROUP.zxcv>
2016-04-01 20:37:44 +1300

    Added automatic Short Name completion.

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var express = require('express');
+var express = require( 'express' );
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require( 'http' ).Server( app );
+var io = require( 'socket.io' )( http );
 var pagedir = __dirname + '/pages';
 
-app.set('port', (process.env.PORT || 3434));
+app.set( 'port', ( process.env.PORT || 3434 ));
 
-app.use(express.static(__dirname + '/public'));
+app.use( express.static( __dirname + '/public' ) );
 
-app.get('/', function(req, res){
+app.get( '/', function( req, res ){
   res.sendFile(pagedir + '/playerdetails.html');
   //res.send('<h1>Hello World</h1>');
 });
 
-http.listen(app.get('port'), function(){
-  console.log('listening on : ' + app.get('port'));
+io.on( 'connection', function( socket ){
+  socket.on( 'name change', function(nameList) {
+    //console.log( 'name change invoked' );
+    //console.log( nameList );
+    var shortName = nameList[1];
+    // This is one of the places where you access the database.
+    if ( nameList[2].length > 0 ) { // If they've entered a last name,
+      shortName = shortName + ' ' + nameList[2].substring( 0,1 );
+    }
+    if ( ( nameList[3] == '' ) || ( nameList[3] == nameList[1] ) ) { // If they haven't changed the short name,
+      io.emit( 'short change', [ nameList[0], shortName ] );
+    }
+  });
+});
+
+http.listen( app.get( 'port' ), function(){
+  console.log( 'listening on : ' + app.get('port') );
 });
