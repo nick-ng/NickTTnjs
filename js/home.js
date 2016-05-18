3758c2b39b332f0563eb4bf3092fb41114f0328d
nick-ng <nicholas.cj.ng@gmail.com>
2016-05-18 16:02:57 +1200

    Updated webpage jQuery version to match that used in Node.js. Added jQuery UI for home page buttons.

--- js/home.js
+++ js/home.js
@@ -1,3 +1,33 @@
 var bgLight = '#eee';
 var bgDark = '#ccc';
 var socket = io();
+
+
+
+$(document).ready(function() {
+  // Make dialogs.
+  $( '#newTournamentDialog' ).dialog({
+    autoOpen: false,
+    resizable: false,
+    //height: 140,
+    width: 400,
+    modal: true,
+    buttons: {
+      'Create': function() {
+        console.log('Getting a new tournament-key');
+        console.log('Redirecting to player details page');
+        //<div id="newTournamentDialog" title="Create a new tournament?">
+        $( this ).dialog( 'option', 'title', 'Creating new tournament...' );
+        $( this ).html( '<p>Please wait</p>' );
+      },
+      'Cancel': function() {
+        $( this ).dialog( 'close' );
+      }
+    }
+  });
+  
+}); // $( document ).ready(function() {
+
+$( '#newTournamentButton' ).click(function() {
+  $( '#newTournamentDialog' ).dialog( 'open' );
+});
